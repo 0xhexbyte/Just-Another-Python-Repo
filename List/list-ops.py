1a5602dfb33fcb7cb4b5ecfee18284c6eca0f945
Mrigendra Soni <36241150+Mrig26@users.noreply.github.com>
2019-11-05 11:16:13 +0530

    Updating

---
+++ List/list-ops.py
@@ -0,0 +1,11 @@
+num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# items in a list can be accessed individually through indexing, beginning from 0
+print(num_list[1])
+# to change a particular item in a list
+num_list[3] = "changed"
+# to slice a list and get the required items
+print(num_list[0:5])
+# to print items at a specific interval from a list
+print(num_list[::2])
+# to print a list in reverse order
+print(num_list[::-1])
---
+++ List/list_unpacking.py
@@ -0,0 +1,12 @@
+swift_list = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+print(swift_list)
+sasta_rasta = ["a", "b", "c", "d", "e", ]
+# we can assign individual list items to variables as such:
+# var1 = swift_list[1]
+# var2 = swift_list[2]
+# var3 = swift_list[3]
+# or we can also do this as:
+first, second, third, four, five = sasta_rasta  # this is called list unpacking
+# or if we want just the first and second, we can do the following:
+f1, f2, *extras = swift_list
+print(extras)
