b1ac881dc16c9d4af71cc507ae83437d5832e349
Mrigendra Soni <36241150+Mrig26@users.noreply.github.com>
2019-11-05 08:28:23 +0530

    Adding current scripts

---
+++ fourth.py
@@ -0,0 +1,6 @@
+def earning(value, by=2):
+    return value*by
+
+
+achieved = earning(1000, 5)
+print(achieved)
---
+++ third.py
@@ -0,0 +1,9 @@
+def letsdothis(passion):
+    print(f"Your passion is {passion}")
+    #mission = passion+"hardwork"
+    return(passion+"hardwork")  # here we can also return variable 'mission'
+
+
+letsdothis("blew blue blew")
+keepgoing = letsdothis("boom")
+print(keepgoing)
