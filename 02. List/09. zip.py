6628bffb6a66ca992eda447dd699987eb3a19e8f
Mrigendra Soni <36241150+Mrig26@users.noreply.github.com>
2019-11-24 00:10:00 +0530

    Updating stuff

--- 02. List/09. zip.py
+++
@@ -1,6 +0,0 @@
-# If we wish to combine 2 or more lists in a list of tuples with items at same index positions clubbed together, we use zip:
-
-list1 = [1, 3, 4, 7, 9, 76, 23]
-list2 = [5, 8, 13, 52, 34, ""]
-
-print(list(zip(list1, list2)))
---
+++ 02. List/09. zip_stack.py
@@ -0,0 +1,28 @@
+# If we wish to combine 2 or more lists in a list of tuples with items at same index positions clubbed together, we use zip:
+
+
+from collections import deque
+list1 = [1, 3, 4, 7, 9, 76, 23]
+list2 = [5, 8, 13, 52, 34, ""]
+
+print(list(zip(list1, list2)))
+
+# Stack Data Structure:
+# They follow LIFO procedure and we can use a list to work as a stack here.
+
+stack_ex = [1, 2, 3]
+stack_ex.append(4)
+print(stack_ex)
+stack_ex.pop()
+print(stack_ex)
+print(stack_ex[-1])
+
+# To check if the 'stack' list is empty or not:
+if not stack_ex:
+    print(stack_ex[-1])
+
+# Queue Data Structure:
+# Queue follows FIFO and can be implemented using a list.
+
+# Module is a collection of code written for our ease. To implement the concept of queue, we'll use a module:
+# deque is a list like sequence optimized for data access near endpoints.
